4d134ede93777138f379bb1126d3ab651731072c
Yulian Hristov <101811818+julHristov@users.noreply.github.com>
2022-10-12 07:31:51 +0300

    Add files via upload

---
+++ 2.tickets.js
@@ -0,0 +1,24 @@
+function ticketSorter(tikets, sorting){
+   class Ticket{
+    constructor(destination, price, sorting){
+       this.destination = destination;
+       this.price = price;
+       this.sorting = sorting;
+    }
+   }
+   const result = [];
+   tikets.map(el =>{
+      let [destination, price, status] = el.split('|');
+      price = Number(price);
+      result.push(new Ticket(destination, price, status));
+   });
+     return result.sort((a, b) => {
+       if(typeof a[sorting] === 'number'){
+         return a[sorting] - b[sorting];
+       }else{
+        return a[sorting].localeCompare(b[sorting]);
+       }
+     });
+}
+console.table(ticketSorter(['Philadelphia|94.20|available', 'New York City|95.99|available', 'New York City|95.99|sold', 'Boston|126.20|departed'], 'destination'));
+console.table(ticketSorter(['Philadelphia|94.20|available', 'New York City|95.99|available', 'New York City|95.99|sold', 'Boston|126.20|departed'], 'status'));
